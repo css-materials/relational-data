0d90e038abb88280c487206cf27dc7795ac9badf
Sabrina Nardin <nardin@uchicago.com>
2024-06-20 11:32:36 -0500

    typos

--- relational-data-solution.R
+++ relational-data-solution.R
@@ -13,7 +13,7 @@ library(tidyverse)
 # QUESTION 1. Is there a relationship between the age of a plane and its departure delays?
 
 # step 1: 
-# inspect the data frames -- which variables do you need to solve the problem?
+# inspect the data frames: which tables and variables do you need to solve the problem?
 # this is a key step, spend some time looking at the tables and the variables they have in common 
 # refer back to the slides or to Ch. 19 from our book (link at the top of this file) for details
 glimpse(flights)
@@ -21,7 +21,7 @@ glimpse(planes)
 
 # step 2: 
 # use mutate() and the appropriate variables to calculate age using the planes table 
-# then select the required variables from the planes table
+# then select() the required variables from the planes table
 # hint: you will notice that there is not an age variable in the data, but we can calculate it:
 # this data is from 2013: we can use this info together with the variable year to calculate age
 plane_ages <- planes %>%
